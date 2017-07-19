2a4e44dea14d0c01300b72ec419adf452339d0ef
冯耀烨 <sky@fengyaoyedeMac-mini.local>
2017-07-19 15:25:42 +0800

    add toolchain

---
+++ xtensa-esp32-elf/lib/gcc/xtensa-esp32-elf/5.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/pi/project/crosstool-NG/.build/src/gcc-5.2.0/configure --build=armv7l-build_unknown-linux-gnueabihf --host=armv7l-build_unknown-linux-gnueabihf --target=xtensa-esp32-elf --prefix=/home/pi/project/crosstool-NG/builds/xtensa-esp32-elf --with-local-prefix=/home/pi/project/crosstool-NG/builds/xtensa-esp32-elf/xtensa-esp32-elf/sysroot --with-sysroot=/home/pi/project/crosstool-NG/builds/xtensa-esp32-elf/xtensa-esp32-elf/sysroot --with-newlib --enable-threads=no --disable-shared --with-pkgversion='crosstool-NG crosstool-ng-1.22.0-61-gab8375a' --disable-__cxa_atexit --enable-cxx-flags='-fno-exceptions -fno-rtti' --with-gmp=/home/pi/project/crosstool-NG/.build/xtensa-esp32-elf/buildtools --with-mpfr=/home/pi/project/crosstool-NG/.build/xtensa-esp32-elf/buildtools --with-mpc=/home/pi/project/crosstool-NG/.build/xtensa-esp32-elf/buildtools --with-isl=/home/pi/project/crosstool-NG/.build/xtensa-esp32-elf/buildtools --with-cloog=/home/pi/project/crosstool-NG/.build/xtensa-esp32-elf/buildtools --with-libelf=/home/pi/project/crosstool-NG/.build/xtensa-esp32-elf/buildtools --enable-lto --enable-target-optspace --without-long-double-128 --disable-libgomp --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-nls --disable-multilib --enable-languages=c,c++ --disable-libstdcxx-verbose";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
